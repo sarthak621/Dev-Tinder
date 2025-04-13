f526dfa8cf0e512b7e8e25a0a369f55f83f5303e
sarthak621 <srivastavsarthak6211@gmail.com>
2025-04-13 23:25:32 +0530

    playing with routes

--- src/practice.js
+++ src/practice.js
@@ -6,11 +6,32 @@ const app=express()
 //     res.send("HA HA HA ")
 // })
 
+
+// works for /abc and /ac also {? is optional chainin g}
+// app.get("/ab?c",(req,res)=>{
+//     res.send({firstname:"Sarthak" , LastName:"Srivastava"})
+// })
+
+// /ab+c -> abc work for /abc and /abbbc also
+// /ab*cd ->abKUCHBHIcd abcd abbbcccd
+
+// /ab(cd)?e -> abcd or abe
+
 //this will only handle GET call to /user
 
-app.get("/user",(req,res)=>{
+app.get("/user/:userId/:name/:password",(req,res)=>{
+    console.log(req.params)
     res.send({firstname:"Sarthak" , LastName:"Srivastava"})
 })
+
+// app.get("/user",(req,res)=>{
+//     console.log(req.query)
+//     res.send({firstname:"Sarthak" , LastName:"Srivastava"})
+// })
+
+// app.get("/user",(req,res)=>{
+//     res.send({firstname:"Sarthak" , LastName:"Srivastava"})
+// })
 
 
 app.post("/user",(req,res)=>{
